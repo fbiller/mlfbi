2bb43375b0057ea0ee4a510e109ba32fcf8b6873
FBi92120 <francois@biller.fr>
2022-04-01 12:41:56 +0200

    new

--- app.py
+++ app.py
@@ -141,28 +141,28 @@ def image_ia(filename):
 def upload_form():
     return render_template('upload.html')
 
-@app.route('/', methods=['POST'])
-def upload_image():
-    print ()
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash('No image selected for uploading')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(UPLOAD_FOLDER, filename))
-        print('upload_image filename: ' + filename)
-        flash('Image successfully uploaded and displayed below')
-        filename_ia = image_ia (filename)
-        return render_template('upload.html', filename="static/uploads/"+filename, filename_ia=filename_ia)
-    else:
-        flash('Allowed image types are -> png, jpg, jpeg, gif')
-        return redirect(request.url)
+# @app.route('/', methods=['POST'])
+# def upload_image():
+#     print ()
+#     if 'file' not in request.files:
+#         flash('No file part')
+#         return redirect(request.url)
+#     file = request.files['file']
+#     if file.filename == '':
+#         flash('No image selected for uploading')
+#         return redirect(request.url)
+#     if file and allowed_file(file.filename):
+#         filename = secure_filename(file.filename)
+#         file.save(os.path.join(UPLOAD_FOLDER, filename))
+#         print('upload_image filename: ' + filename)
+#         flash('Image successfully uploaded and displayed below')
+#         filename_ia = image_ia (filename)
+#         return render_template('upload.html', filename="static/uploads/"+filename, filename_ia=filename_ia)
+#     else:
+#         flash('Allowed image types are -> png, jpg, jpeg, gif')
+#         return redirect(request.url)
 
 
 if __name__ == "__main__":
     app.secret_key = "secret key"
-    app.run(debug=True)
+    app.run()
